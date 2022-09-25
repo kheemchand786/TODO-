2a51a4941b640df79f38be9cd258b4ed275c39ee
Hiten Kumar <hitenkumarvendani@gmail.com>
2022-09-26 00:59:42 +0500

    HELLO WORLD EXPRESS SERVER

--- server.mjs
+++ server.mjs
@@ -1,10 +1,37 @@
 import express from 'express'
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-    console.log("response from Kheemchand")
-    res.send('Hello World! from Kheemchanad')
+
+let todos = [];
+
+
+
+app.use(express.json());
+app.post('/todo', (req, res) => {
+
+    todos.push(req.body.text);
+
+    res.send({
+        message: "your todo is saved",
+        data: todos
+    })
+    console.log('someone is asking for water')
+    res.send('here is your water')
+})
+
+
+
+app.get('/pizza', (req, res) => {
+    console.log('someone is asking for pizza')
+    res.send('here is your pizza')
+})
+
+
+
+app.get('/icecream', (req, res) => {
+    console.log('someone is asking for icecream')
+    res.send('here is your icecream')
 })
 
 app.listen(port, () => {
