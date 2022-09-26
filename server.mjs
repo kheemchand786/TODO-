d00192dc4aa53a8c0a9044c18426f05566564493
jatindarkhatry <khatrij784@gmail.com>
2022-09-26 20:15:57 +0500

    TODO-ASSIGNMENT

--- server.mjs
+++ server.mjs
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -8,6 +9,7 @@ let todos = [];
 
 
 app.use(express.json());
+app.use(cros())
 app.post('/todo', (req, res) => {
 
     todos.push(req.body.text);
@@ -16,22 +18,21 @@ app.post('/todo', (req, res) => {
         message: "your todo is saved",
         data: todos
     })
-    console.log('someone is asking for water')
-    res.send('here is your water')
+   
 })
 
 
 
-app.get('/pizza', (req, res) => {
-    console.log('someone is asking for pizza')
-    res.send('here is your pizza')
+
+
+
+
+app.get('/todos', (req, res) => {
+    
+    res.send({
+        message:"here is your list",
+        data:todos
 })
-
-
-
-app.get('/icecream', (req, res) => {
-    console.log('someone is asking for icecream')
-    res.send('here is your icecream')
 })
 
 app.listen(port, () => {
